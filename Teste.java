79962f16aa03695882b31c5bb721a6f7369899de
Rafael Madruga <104838747+RafaelElGon@users.noreply.github.com>
2025-04-04 11:03:15 -0300

    aaaaaaaaaaaa

--- Teste.java
+++ Teste.java
@@ -1,7 +1,5 @@
 class Teste {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-
-        System.out.print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
     }
 }
